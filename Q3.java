72f8e8696de13360244f90c9fc9b1ab7e6aaa20f
KeeBoonHwee <2717921k@student.gla.ac.uk>
2022-01-17 19:50:26 +0800

    Completed Q3

--- Q3.java
+++ Q3.java
@@ -2,6 +2,16 @@ public class Q3
 {
     public static void main(String[] args)
     {
-        
+        long totalMilliseconds = System.currentTimeMillis();
+        long totalSeconds = totalMilliseconds / 1000;
+
+        long currentSecond = totalSeconds % 60;
+        long totalMinutes  = (totalSeconds / 60) + (8 * 60); // Singapore timezone is GMT+8
+
+        long currentMinute = totalMinutes % 60;
+        long totalHours    = totalMinutes / 60;
+
+        long currentHour   = totalHours % 24;
+        System.out.print("Current SGT time is " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT+8");
     }
 }
